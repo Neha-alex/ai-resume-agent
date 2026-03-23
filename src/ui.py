bc3d057e0234188984d7c5023c32a81438a35289
Alex <nealex@deloitte.com>
2026-03-23 13:40:13 +0530

    updating requirements

--- src/ui.py
+++ src/ui.py
@@ -4,6 +4,9 @@ from .agent import chat
 def start_ui():
     interface = gr.ChatInterface(
         fn = lambda message, history:chat(message, history),
-        title = "AI Resume Agent"
+        title = "AI Resume Agent",
+        description="Hi, This is Neha Alex's AI Resume Assistant \n\n"
+        "You can ask me anything about Neha's experience, skills, or projects. \n"
+        "If you're a recruiter, feel free to leave your name and email - Neha will get back to you!"
     )
     interface.launch()
